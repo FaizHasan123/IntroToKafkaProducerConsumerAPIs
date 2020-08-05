c479fa294d8b0cc2332ca18b8c69f23de430e0e9
Faiz Hasan <43991774+FaizHasan123@users.noreply.github.com>
2020-08-05 11:04:31 +0400

    Add files via upload

--- src/main/java/consumerDemo.java
+++ src/main/java/consumerDemo.java
@@ -1,31 +1,33 @@
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Properties;
+
 import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.*;
 
 class consumerDemo {
 
 	public static void main(String args[]) throws InterruptedException {
 		Properties consumerProps = new Properties();
-		consumerProps.put("bootstrap.servers", "localhost:9092");
+		 consumerProps.put("bootstrap.servers", "broker-5-rgtvthnhvdqfxgjw.kafka.svc06.us-south.eventstreams.cloud.ibm.com:9093,broker-2-rgtvthnhvdqfxgjw.kafka.svc06.us-south.eventstreams.cloud.ibm.com:9093,broker-4-rgtvthnhvdqfxgjw.kafka.svc06.us-south.eventstreams.cloud.ibm.com:9093,broker-1-rgtvthnhvdqfxgjw.kafka.svc06.us-south.eventstreams.cloud.ibm.com:9093,broker-0-rgtvthnhvdqfxgjw.kafka.svc06.us-south.eventstreams.cloud.ibm.com:9093,broker-3-rgtvthnhvdqfxgjw.kafka.svc06.us-south.eventstreams.cloud.ibm.com:9093");
+		 consumerProps.put("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"token\" password=\"oZ17lr1q6tH52OrnJ913uU_XGhSn0MAMpHj5nOEqgFaN\";");
+		 consumerProps.put("security.protocol", "SASL_SSL");
+		 consumerProps.put("sasl.mechanism", "PLAIN");
+		 consumerProps.put("ssl.protocol", "TLSv1.2");
+		 consumerProps.put("ssl.enabled.protocols", "TLSv1.2");
+		 consumerProps.put("ssl.endpoint.identification.algorithm", "HTTPS");
+		 consumerProps.put("group.id", "KafkaExampleConsumer");
+		 consumerProps.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		 consumerProps.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
-		consumerProps.put("group.id", "KafkaExampleConsumer");
-
-		consumerProps.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-
-		consumerProps.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-
-		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(consumerProps);
-		consumer.subscribe(Collections.singletonList("test"));
-		while (true) {
-			ConsumerRecords<String, String> records = consumer.poll(1000);
-			records.forEach(record -> {
-				System.out.println(record.value());
-			});
-		}
+		 consumerProps.put("group.id", "G1");
+		 Consumer<String, String> consumer = new KafkaConsumer<>(consumerProps);
+		 consumer.subscribe(Arrays.asList("getting-started"));
+		 while (true) {
+		    ConsumerRecords<String, String> records = consumer.poll(100);
+		    for (ConsumerRecord<String, String> record : records)
+		      System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+		 }
 	}
 }
--- src/main/java/producerDemo.java
+++ src/main/java/producerDemo.java
@@ -6,14 +6,25 @@ class ProducerDemo {
 
 	public static void main(String args[]) throws InterruptedException {
 		Properties producerProps = new Properties();
-		producerProps.put("bootstrap.servers", "localhost:9092");
+		producerProps.put("bootstrap.servers", "broker-5-rgtvthnhvdqfxgjw.kafka.svc06.us-south.eventstreams.cloud.ibm.com:9093, "
+				                             + "broker-2-rgtvthnhvdqfxgjw.kafka.svc06.us-south.eventstreams.cloud.ibm.com:9093, "
+				                             + "broker-4-rgtvthnhvdqfxgjw.kafka.svc06.us-south.eventstreams.cloud.ibm.com:9093, "
+				                             + "broker-1-rgtvthnhvdqfxgjw.kafka.svc06.us-south.eventstreams.cloud.ibm.com:9093, "
+				                             + "broker-0-rgtvthnhvdqfxgjw.kafka.svc06.us-south.eventstreams.cloud.ibm.com:9093, "
+				                             + "broker-3-rgtvthnhvdqfxgjw.kafka.svc06.us-south.eventstreams.cloud.ibm.com:9093");
+		producerProps.put("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"token\" password=\"oZ17lr1q6tH52OrnJ913uU_XGhSn0MAMpHj5nOEqgFaN\";");
+		producerProps.put("security.protocol", "SASL_SSL");
+		producerProps.put("sasl.mechanism", "PLAIN");
+		producerProps.put("ssl.protocol", "TLSv1.2");
+		producerProps.put("ssl.enabled.protocols", "TLSv1.2");
+		producerProps.put("ssl.endpoint.identification.algorithm", "HTTPS");
 		producerProps.put("acks", "all");
 		producerProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 		producerProps.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
 		Producer<String, String> producer = new KafkaProducer<>(producerProps);
 		 for (int i = 0; i < 1000; i++) {
-		     producer.send(new ProducerRecord<String, String>("test", Integer.toString(i), Integer.toString(i)));
+		     producer.send(new ProducerRecord<String, String>("getting-started", Integer.toString(i), Integer.toString(i)));
 		     Thread.sleep(5000);
 		 }
 		 producer.close();
